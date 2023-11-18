39fb7546d3c4f5177452bea3dc5265dac48186d5
giorgi mikeladze <mikeladzegiorgi538@outlook.com>
2023-11-18 11:49:56 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int numb;
+Console.Write("Enter integer value: ");
+bool is_parsed = Int32.TryParse(Console.ReadLine(), out numb);
+
+if (is_parsed && numb>=1)
+{
+    int sum = 0;
+    for (int i = 0; i <= numb; i++) {
+        if (i%2==1) {
+            sum += i;
+        }
+        
+    }
+    Console.WriteLine($"sum of the odd numbers from 1 to {numb} is: {sum}");
+    
+   
+}
+else { 
+
+}
